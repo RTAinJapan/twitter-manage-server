18bb6828b3499bf522257a337b7c7a7aa56cb360
Keiichiro Amemiya <hoishinxii@gmail.com>
2024-07-13 10:08:46 +0900

    ah

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ server.get("/", () => {
 });
 
 try {
-	await server.listen({ port: 3000 });
+	await server.listen({ host: "0.0.0.0", port: 3000 });
 } catch (error) {
 	console.error(error);
 	process.exit(1);
