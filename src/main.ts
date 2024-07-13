0591530f8a70a32c37cd419b862c3e2dd507c4a1
Keiichiro Amemiya <hoishinxii@gmail.com>
2024-07-13 14:28:59 +0900

    counter test

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ server.get("/", () => {
 	return { ok: true };
 });
 
+let count = 0;
+server.get('/counter', () => {
+	count += 1;
+	return { count };
+})
 try {
 	await server.listen({ host: "0.0.0.0", port: 3000 });
 } catch (error) {
